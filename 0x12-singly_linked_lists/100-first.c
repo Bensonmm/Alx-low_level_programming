cfcfe0c908fa02d058771cf8126e11fee519bc80
Bensonmm <27972639+Bensonmm@users.noreply.github.com>
2023-07-12 18:54:47 +0300

    0x12. C - Singly linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+* myStartupFun - Function to execute before main()
+* This function is a constructor that will be automatically called before main
+* is executed. It prints the message "You're beat! and yet, you must allow,"
+* followed by "I bore my house upon my back!" to the console.
+*/
+void myStartupFun(void) __attribute__ ((constructor));
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
